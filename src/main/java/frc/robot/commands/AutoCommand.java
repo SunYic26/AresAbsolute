9c16dce95d9fc3d42ed9f53a50b3e009a7529673
Ethan <ethanmwgo@gmail.com>
2025-01-06 09:16:43 -0800

    restructuring to increase scalability

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -13,9 +13,10 @@ import java.util.HashMap;
 public class AutoCommand {
     Command autoCommand;
     public String name;
-    private String[] potentialContinuations;   
+    private EndPoint end;
     private static HashMap<String, AutoCommand> pathKeys = new HashMap<String, AutoCommand>();
     private static ArrayList<AutoCommand> autoList2 = new ArrayList<AutoCommand>();
+
     //if you have questions ask ethan
     public static void loadAutos(){
         pathKeys.put("Test1", Test1());
@@ -25,19 +26,25 @@ public class AutoCommand {
         pathKeys.put("meterForwardTest", meterForwardTest());
     }
 
-    public AutoCommand(String name, Command autoCommand, String[] potentialContinuations){
+    public AutoCommand(String name, Command autoCommand, EndPoint endPoint){
         this.name = name;
         this.autoCommand = autoCommand;
-        this.potentialContinuations = potentialContinuations;
+        this.end = end;
     }
 
-    public static void clearContinuations(){
-        autoList2.clear();
-    }
-
-    public static void fillAutosList(AutoCommand auto){
-        for(String name : auto.potentialContinuations){
-            autoList2.add(getAuto(name));
+    private enum EndPoint {
+        GENERIC(new AutoCommand[]{}), //use for wait commands, etc. Any paths that can lead into any other path
+        S1(new AutoCommand[]{}), //Source areas
+        S2(new AutoCommand[]{}),
+        R1(new AutoCommand[]{}), //Reef scoring areas
+        R2(new AutoCommand[]{}),
+        R3(new AutoCommand[]{}),
+        R4(new AutoCommand[]{}),
+        R5(new AutoCommand[]{}),
+        R6(new AutoCommand[]{});
+        private AutoCommand[] branches; //this stores all of the paths that start at this endpoint
+        private EndPoint(AutoCommand[] branches){
+            this.branches = branches;
         }
     }
 
@@ -45,6 +52,15 @@ public class AutoCommand {
         return this.autoCommand;
     }
 
+    public static void clearContinuations(){
+        autoList2.clear();
+    }
+
+    public static void fillAutosList(AutoCommand auto){
+        for(AutoCommand command : auto.end.branches){
+            autoList2.add(command);
+        }
+    }
 
 
     public static ArrayList<AutoCommand> getPotentialContinuations() {
@@ -52,23 +68,23 @@ public class AutoCommand {
     }
 
     public static AutoCommand Test1(){
-        return new AutoCommand("Test1", Autos.Test1(), new String[]{"Test2", "Test3", "Test4", "meterForwardTest"});
+        return new AutoCommand("Test1", Autos.Test1(), EndPoint.GENERIC);
     }
 
     public static AutoCommand Test2(){
-        return new AutoCommand("Test2", Autos.Test2(), new String[]{"Test1", "Test3", "Test4"});
+        return new AutoCommand("Test2", Autos.Test2(), EndPoint.GENERIC);
     }
 
     public static AutoCommand Test3() {
-        return new AutoCommand("Test3", Autos.Test3(), new String[]{"Test1", "Test2", "Test4"});
+        return new AutoCommand("Test3", Autos.Test3(), EndPoint.GENERIC);
     }
 
     public static AutoCommand Test4() {
-        return new AutoCommand("Test4", Autos.Test4(), new String[]{"Test1", "Test2", "Test3"});
+        return new AutoCommand("Test4", Autos.Test4(), EndPoint.GENERIC);
     }
 
     public static AutoCommand meterForwardTest() {
-        return new AutoCommand("meterForwardTest", Autos.meterForwardTest(), new String[]{"Test1", "Test2", "Test3", "Test4"});
+        return new AutoCommand("meterForwardTest", Autos.meterForwardTest(), EndPoint.GENERIC);
     }
 
     public static AutoCommand getAuto(String name){
