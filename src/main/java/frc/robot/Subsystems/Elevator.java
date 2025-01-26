fb3456b6819964318499c7c37ff0a1410d628af2
Ethan <ethanmwgo@gmail.com>
2025-01-25 23:23:42 -0800

    a

--- src/main/java/frc/robot/Subsystems/Elevator.java
+++ src/main/java/frc/robot/Subsystems/Elevator.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -46,17 +47,17 @@ public class Elevator extends SubsystemBase {
   public Elevator() {
     leader = new TalonFX(Constants.HardwarePorts.elevatorLeaderId);
     follower = new TalonFX(Constants.HardwarePorts.elevatorFollowerId);
-    configMotor(leader, false, NeutralModeValue.Brake);
-    configMotor(follower, false, NeutralModeValue.Brake);
+    configMotor(leader, InvertedValue.CounterClockwise_Positive, NeutralModeValue.Brake);
+    configMotor(follower, InvertedValue.Clockwise_Positive, NeutralModeValue.Brake);
 
-    follower.setControl(new Follower(Constants.HardwarePorts.elevatorLeaderId, false));
+    follower.setControl(new Follower(Constants.HardwarePorts.elevatorLeaderId, true));
   }
 
-  private void configMotor(TalonFX motor, boolean inverted, NeutralModeValue neutralMode){
+  private void configMotor(TalonFX motor, InvertedValue direction, NeutralModeValue neutralMode){
     motor.setNeutralMode(neutralMode);
     TalonFXConfiguration config = new TalonFXConfiguration();
     CurrentLimitsConfigs currentLimitsConfigs = new CurrentLimitsConfigs();
-    
+    config.MotorOutput.Inverted = direction;
     currentLimitsConfigs.SupplyCurrentLimit = Constants.CurrentLimits.elevatorContinuousCurrentLimit;
     currentLimitsConfigs.SupplyCurrentLimitEnable = true;
     currentLimitsConfigs.StatorCurrentLimit = Constants.CurrentLimits.elevatorPeakCurrentLimit;
@@ -64,7 +65,8 @@ public class Elevator extends SubsystemBase {
 
     config.CurrentLimits = currentLimitsConfigs;
 
-    motor.optimizeBusUtilization();
+  
+    // motor.optimizeBusUtilization();
   }
 
   public double getPosition(){
@@ -79,6 +81,10 @@ public class Elevator extends SubsystemBase {
     return leader.getVelocity().getValueAsDouble();
   }
 
+  public double getAcceleration() {
+    return leader.getAcceleration().getValueAsDouble();
+  }
+
   public void setVoltage(double voltage){
     leader.setVoltage(voltage);
   }
@@ -86,5 +92,7 @@ public class Elevator extends SubsystemBase {
   @Override
   public void periodic() {
     SmartDashboard.putNumber("elevator position", getPosition());
+    SmartDashboard.putNumber("elevator acceleration", getAcceleration());
+    SmartDashboard.putNumber("elevator velocity", getVelocity());
   }
 }
