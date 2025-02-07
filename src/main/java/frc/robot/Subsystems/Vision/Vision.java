d02f980ad5a550f78850c1b46f2f6af4cf902723
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2025-02-07 10:23:10 -0800

    Logic fix

--- src/main/java/frc/robot/Subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/Subsystems/Vision/Vision.java
@@ -19,6 +19,7 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
